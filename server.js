364f5007a7f0b73e8644823cd9df70b2dca8bc64
Katheryna <k.karpo.a@gmail.com>
2019-09-12 16:48:40 +0300

    mongo & mongoose connection

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 
+const connectDB = require("./config/db");
 const app = express();
+//connect our database (mongoDB)
+connectDB();
 app.get("/", (req, res) => res.send("API RUNING"));
 
 const PORT = process.env.PORT || 5000;
